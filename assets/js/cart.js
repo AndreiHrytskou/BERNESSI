c08d891096c127eb099d10c10773711a5325aa54
Andrei Hrytskou <andreihrytskou@gmail.com>
2023-02-19 19:29:47 +0300

    считает сумму добавленых товаров

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -4,6 +4,8 @@ const cartWrapper = document.querySelector(".cart-wrapper");
 const div = document.createElement("div");
 const resetCart = document.querySelector(".reset-cart");
 const cartItem = document.querySelectorAll(".cart__item");
+const itemPrice = document.querySelectorAll(".item-price > .price");
+let sum = document.querySelector(".sum");
 
 cart.addEventListener("click", () => {
   div.className = "background";
@@ -22,4 +24,10 @@ resetCart.addEventListener("click", () => {
   cartItem.forEach((e) => {
     e.remove();
   });
+});
+let arr = [];
+itemPrice.forEach((e) => {
+  arr.push(+e.innerText);
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  sum.innerText = arr.reduce(reducer);
 });
