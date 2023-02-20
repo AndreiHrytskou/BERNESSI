0c175203879d2567f155330c393833f51b219021
Andrei Hrytskou <andreihrytskou@gmail.com>
2023-02-20 17:01:32 +0300

    cart-page: made: считает сумму всех товаров

--- assets/js/cart-sidebar.js
+++ assets/js/cart-sidebar.js
@@ -12,3 +12,19 @@ hideBlock.forEach((e) => {
     e.childNodes[3].classList.toggle("rotate");
   });
 });
+
+//  sum
+const priceItems = document.querySelectorAll(
+  ".price-product > .item-price > .price"
+);
+const total = document.querySelector(".total-sum > .sum");
+const tax = document.querySelector(".tax-price > .price");
+const subtotal = document.querySelector(".subtotal-price > .price");
+let arrProduct = [];
+priceItems.forEach((e) => {
+  arrProduct.push(+e.innerText);
+  const reducer = (acc, currentValue) => acc + currentValue;
+  subtotal.innerText = arrProduct.reduce(reducer);
+});
+let summ = Number(subtotal.innerText) + Number(tax.innerText);
+total.innerText = summ.toFixed(2);
