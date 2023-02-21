49e49d95d5d38f93c003f436476d043ace6e4fef
Andrei Hrytskou <andreihrytskou@gmail.com>
2023-02-21 18:54:59 +0300

    add address-book page & made js from check page

--- assets/js/checkAccount.js
+++ assets/js/checkAccount.js
@@ -2,7 +2,7 @@ const title = document.querySelector(".title");
 const radio = document.querySelectorAll(".custom-radio");
 const labelItem = document.querySelectorAll(".label");
 labelItem.forEach((e) => {
-  if (title.innerHTML == e.innerHTML) {
+  if (title.innerHTML == e.innerHTML || title.title == e.innerHTML) {
     e.previousElementSibling.checked = true;
   }
 });
