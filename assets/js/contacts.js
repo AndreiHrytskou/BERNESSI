283fd9d5602b9019b48faec814230d3984f15517
Andrei Hrytskou <andreihrytskou@gmail.com>
2023-03-03 10:32:11 +0300

    адаптив страница contacts

---
+++ assets/js/contacts.js
@@ -0,0 +1,16 @@
+const selectOutput = document.querySelector(".select-output");
+const outputsize = document.querySelector(".outputsize");
+const selectTitle = document.querySelector(".select-title");
+const selectOption = document.querySelectorAll(".select__option");
+const selectArrow = document.querySelector(".select-arrow");
+selectOutput.addEventListener("click", () => {
+  selectTitle.classList.toggle("select-active");
+  selectArrow.classList.toggle("rotate");
+  selectOption.forEach((e) => {
+    e.addEventListener("click", () => {
+      outputsize.innerHTML = e.value;
+      selectTitle.classList.remove("select-active");
+      selectArrow.classList.toggle("rotate");
+    });
+  });
+});
