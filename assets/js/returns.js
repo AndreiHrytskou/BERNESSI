a99a22b319338202195e733e01b2fa74980125ba
Andrei Hrytskou <andreihrytskou@gmail.com>
2023-03-07 17:32:39 +0300

    done returns page

--- assets/js/returns.js
+++ assets/js/returns.js
@@ -2,5 +2,20 @@ const tabs = document.querySelectorAll(".returns-tab");
 const content = document.querySelectorAll(".return");
 
 tabs.forEach((e) => {
-  e.addEventListener("click", () => {});
+  e.addEventListener("click", () => {
+    tabs.forEach((el) => {
+      if (el.classList.contains("returns-tab")) {
+        el.classList.remove("tab-active");
+      }
+    });
+    e.classList.add("tab-active");
+    const btn = e.dataset.btn;
+    console.log(btn);
+    content.forEach((c) => {
+      c.classList.remove("return-active");
+      if (c.dataset.content === btn) {
+        c.classList.add("return-active");
+      }
+    });
+  });
 });
