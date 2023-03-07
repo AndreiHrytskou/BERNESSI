85715fa2f8ad600bd88a4681a5a07129a3ed70c4
Andrei Hrytskou <andreihrytskou@gmail.com>
2023-03-07 14:31:55 +0300

    поправил container

--- assets/js/returns.js
+++ assets/js/returns.js
@@ -1,21 +1,6 @@
-const tabs = document.querySelector(".returns-tabs");
+const tabs = document.querySelectorAll(".returns-tab");
 const content = document.querySelectorAll(".return");
 
-const changeClass = (el) => {
-  //   for (let i = 0; i < tabs.children.length; i++) {
-  //     tabs.children[i].classList.remove("tab-active");
-  //   }
-  //   el.classList.add("tab-active");
-  console.log(tabs);
-};
-
-tabs.addEventListener("click", (e) => {
-  const currTab = e.target.dataset.btn;
-  changeClass(e.target);
-  for (let i = 0; i < content.length; i++) {
-    content[i].classList.remove("active");
-    if (content[i].dataset.content === currTab) {
-      content[i].classList.add("active");
-    }
-  }
+tabs.forEach((e) => {
+  e.addEventListener("click", () => {});
 });
