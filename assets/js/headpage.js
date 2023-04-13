84ab1ef0d1b64f2307ddfb6e8bb4faecf11c8bb5
Andrei Hrytskou <andreihrytskou@gmail.com>
2023-04-13 19:27:46 +0300

    head page animated

--- assets/js/headpage.js
+++ assets/js/headpage.js
@@ -98,42 +98,67 @@ document.addEventListener("DOMContentLoaded", function () {
   const header = document.querySelector(".header");
   const baner = document.querySelector(".baner");
   const banerImg = document.querySelector(".baner__img");
-  if (window.innerWidth >= 992) {
+  if (window.outerWidth > 992) {
     banerImg.style.height = `calc(100vh - ${header.clientHeight}px)`;
     baner.style.height = `calc(100vh - ${header.clientHeight}px)`;
-    baner.style.marginTop = `${header.clientHeight}px`;
+    //  baner.style.marginTop = `${header.clientHeight}px`;
     window.addEventListener("scroll", () => {
       baner.classList.add("baner__scroll");
       if (pageYOffset < 600) {
         baner.style.height = banerImg.clientHeight + "px";
         banerImg.style.height = `100%`;
-        let r = pageYOffset + 50 + "px";
-        baner.style.maxWidth = "95%";
-        baner.style.transform = `translateX(-50%)`;
-        baner.style.position = "fixed";
+        //   let r = pageYOffset + 50 + "px";
+        //   baner.style.maxWidth = "95%";
+        //   baner.style.transform = `translateX(-50%)`;
+        //   baner.style.position = "fixed";
         baner.style.top = header.clientHeight + "px";
-        baner.style.left = "50%";
+        //   baner.style.left = "50%";
         baner.style.marginTop = 0;
       }
 
       //   background
       const background = document.querySelector(".main__background");
       let elem = background.getBoundingClientRect();
-      let top = elem.top - window.pageYOffset;
-      console.log(top);
+      let top = elem.top - document.documentElement.scrollTop;
+      let middle = top + background.clientHeight;
+      let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      let scroll = scrollTop + background.clientHeight - 100;
+      let topBlock = elem.top + scrollTop;
+      let bottomBlock = elem.bottom + scrollTop;
 
-      if (pageYOffset > 500) {
-        let i = pageYOffset * 0.0008;
-        baner.style.top = elem.top - banerImg.clientHeight - 27 + "px";
+      // console.log(middle);
+
+      if (scroll > topBlock) {
+        let i = pageYOffset * 0.001;
+        //   baner.style.position = "relative";
         background.style.opacity = i;
       }
-      if (pageYOffset > 1400) {
+      if (middle < 0) {
         let b = elem.bottom * 0.0014;
         background.style.opacity = b;
-        console.log(elem.bottom * 0.001);
       }
-      background.style.marginTop =
-        header.clientHeight + banerImg.clientHeight + 600 + "px";
+      // background.style.marginTop =
+      //   header.clientHeight + banerImg.clientHeight + 600 + "px";
+      const catalog = document.querySelector(".catalog1");
+      const category = document.querySelector(".category");
+      const banner = document.querySelector(".banner");
+      let el = catalog.getBoundingClientRect();
+      let cat = category.getBoundingClientRect();
+      let bann = banner.getBoundingClientRect();
+      // let elTop = el.top + scrollTop - 200;
+      // let elTopCat = cat.top + scrollTop - 200;
+      let elScroll = scrollTop + catalog.clientHeight - 100;
+      let elScrollCat = scrollTop + category.clientHeight - 100;
+      let elScrollBan = scrollTop + banner.clientHeight - 100;
+      if (elScroll > bottomBlock + 200) {
+        catalog.style.transform = "translateY(0)";
+      }
+      if (elScrollCat > el.bottom + scrollTop + 100) {
+        category.style.transform = "translateY(0)";
+      }
+      if (elScrollBan > cat.bottom + scrollTop + 100) {
+        banner.style.transform = "translateY(0)";
+      }
     });
   }
 });
