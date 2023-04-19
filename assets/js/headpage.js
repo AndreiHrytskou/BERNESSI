a0df786ebf7f1178af70632a75cfc4d3eb7919ac
Andrei Hrytskou <andreihrytskou@gmail.com>
2023-04-19 16:26:10 +0300

    head page

--- assets/js/headpage.js
+++ assets/js/headpage.js
@@ -2,6 +2,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
+    spaceBetween: 30,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
