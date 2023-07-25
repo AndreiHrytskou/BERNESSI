7cf104c300a9147fac7f820738afb28c07bda447
Andrei Hrytskou <andreihrytskou@gmail.com>
2023-07-25 22:01:35 +0200

    new menu

--- assets/js/header.js
+++ assets/js/header.js
@@ -4,7 +4,42 @@ document.addEventListener("DOMContentLoaded", function () {
     const page = document.querySelector(".page");
     page.style.paddingTop =
       document.querySelector(".header").clientHeight + "px";
+
   }
+
+  let lastScroll = 0;
+  const defaultOffset = 200;
+  const header = document.querySelector(".header");
+
+  const scrollPosition = () =>
+    window.pageYOffset || document.documentElement.scrollTop;
+  const containHide = () => header.classList.contains("hide");
+
+  if(window.outerWidth > 576){
+	window.addEventListener("scroll", () => {
+		if (
+			scrollPosition() > lastScroll &&
+			!containHide() &&
+			scrollPosition() > defaultOffset
+		) {
+			//scroll down
+			header.classList.add("hide");
+			header.style.transform = `translateY(-${header.clientHeight}px)`;
+		} else if (scrollPosition() < lastScroll && containHide()) {
+			//scroll up
+			header.classList.remove("hide");
+			header.style.transform = `translateY(0)`;
+		}
+
+		lastScroll = scrollPosition();
+	});
+	
+  }
+ if(window.outerWidth < 576){
+	const page = document.querySelector(".page");
+    page.style.paddingTop =
+      document.querySelector(".header").clientHeight + "px";
+ }
   const burger = document.querySelector(".burger-menu");
   const menuHide = document.querySelector(".menu");
   burger.addEventListener("click", () => {
@@ -13,28 +48,46 @@ document.addEventListener("DOMContentLoaded", function () {
     menuHide.classList.toggle("menu-active");
   });
   if (window.innerWidth >= 1201) {
-    const menuItem = document.querySelectorAll(".menu__item");
+    const menuItem = document.querySelectorAll(".header .menu__item");
     const submenu = document.querySelectorAll(".submenu__wrapper");
     menuItem.forEach((e) => {
-      e.addEventListener("click", () => {
-        //   let category = e.dataset.cat;
-        //   submenu.forEach((el) => {
-        //     let subcategory = el.dataset.cat;
-        //     if (!e.classList.contains("menu__active")) {
-        //       e.classList.remove("menu__active");
-        //     }
-        //   });
-        e.classList.toggle("menu__active");
-      });
-    });
-  }
+		// console.log(e.childNodes);
+		if(e.childNodes.length > 3){
+			e.childNodes[1].removeAttribute("href");
+		
+		 e.addEventListener("click", () => {
+				let category = e.dataset.cat;
+				submenu.forEach((el) => {
+					let subcategory = el.dataset.cat;
+					if (category != subcategory) {
+						el.classList.remove("submenu__active");
+					} else {
+						if (!e.classList.contains("menu__active")) {
+							el.classList.toggle("submenu__active");				  
+						} else {
+							el.classList.remove("submenu__active");
+						}
+					}
+				});
+			});
+		}
+		document.addEventListener("click", (e)=>{
+			let target = e.target;
 
+			if(target != document.querySelector(".menu__block-title")){
+				document.querySelector(".submenu__wrapper").classList.remove("submenu__active")
+			}
+		})
+    });
+	}
   const menuItem = document.querySelectorAll(".menu__block-span");
   const submenuItem = document.querySelectorAll(".submenu__item-span");
-  const arrowBack = document.querySelectorAll(".arrow-back");
-  const arrowBack2 = document.querySelectorAll(".mobile2-arrow");
   if (window.innerWidth <= 1200) {
     menuItem.forEach((e) => {
+		console.log(e.parentNode.childNodes.length);
+		if(e.parentNode.childNodes.length > 3){
+			e.removeAttribute("href");
+		
       e.addEventListener("click", () => {
         e.parentElement.classList.add("submenu-active");
         e.parentNode.childNodes[3].childNodes[1].addEventListener(
@@ -44,6 +97,7 @@ document.addEventListener("DOMContentLoaded", function () {
           }
         );
       });
+		}
     });
     submenuItem.forEach((e) => {
       e.addEventListener("click", () => {
@@ -64,33 +118,33 @@ document.addEventListener("DOMContentLoaded", function () {
     document.body.classList.add("rtl");
   }
 
-  //   smooth scroll
+     //   smooth scroll
 
-  SmoothScroll({
-    // Время скролла 400 = 0.4 секунды
-    animationTime: 800,
-    // Размер шага в пикселях
-    stepSize: 50,
+     SmoothScroll({
+       // Время скролла 400 = 0.4 секунды
+       animationTime: 800,
+       // Размер шага в пикселях
+       stepSize: 50,
 
-    // Дополнительные настройки:
+       // Дополнительные настройки:
 
-    // Ускорение
-    accelerationDelta: 30,
-    // Максимальное ускорение
-    accelerationMax: 2,
+       // Ускорение
+       accelerationDelta: 30,
+       // Максимальное ускорение
+       accelerationMax: 2,
 
-    // Поддержка клавиатуры
-    keyboardSupport: true,
-    // Шаг скролла стрелками на клавиатуре в пикселях
-    arrowScroll: 50,
+       // Поддержка клавиатуры
+       keyboardSupport: true,
+       // Шаг скролла стрелками на клавиатуре в пикселях
+       arrowScroll: 50,
 
-    // Pulse (less tweakable)
-    // ratio of "tail" to "acceleration"
-    pulseAlgorithm: true,
-    pulseScale: 4,
-    pulseNormalize: 1,
+       // Pulse (less tweakable)
+       // ratio of "tail" to "acceleration"
+       pulseAlgorithm: true,
+       pulseScale: 4,
+       pulseNormalize: 1,
 
-    // Поддержка тачпада
-    touchpadSupport: true,
-  });
+       // Поддержка тачпада
+       touchpadSupport: true,
+     });
 });
