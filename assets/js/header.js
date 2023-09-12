af3feef25e979b74c5a228738c1c160cdbe2b676
Andrei Hrytskou <andreihrytskou@gmail.com>
2023-09-12 23:33:34 +0200

    promo code page

--- assets/js/header.js
+++ assets/js/header.js
@@ -131,32 +131,35 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
      //   smooth scroll
+// if(SmoothScroll){
 
-     SmoothScroll({
-       // Время скролла 400 = 0.4 секунды
-       animationTime: 800,
-       // Размер шага в пикселях
-       stepSize: 50,
 
-       // Дополнительные настройки:
+//      SmoothScroll({
+//        // Время скролла 400 = 0.4 секунды
+//        animationTime: 800,
+//        // Размер шага в пикселях
+//        stepSize: 50,
 
-       // Ускорение
-       accelerationDelta: 30,
-       // Максимальное ускорение
-       accelerationMax: 2,
+//        // Дополнительные настройки:
 
-       // Поддержка клавиатуры
-       keyboardSupport: true,
-       // Шаг скролла стрелками на клавиатуре в пикселях
-       arrowScroll: 50,
+//        // Ускорение
+//        accelerationDelta: 30,
+//        // Максимальное ускорение
+//        accelerationMax: 2,
 
-       // Pulse (less tweakable)
-       // ratio of "tail" to "acceleration"
-       pulseAlgorithm: true,
-       pulseScale: 4,
-       pulseNormalize: 1,
+//        // Поддержка клавиатуры
+//        keyboardSupport: true,
+//        // Шаг скролла стрелками на клавиатуре в пикселях
+//        arrowScroll: 50,
 
-       // Поддержка тачпада
-       touchpadSupport: true,
-     });
+//        // Pulse (less tweakable)
+//        // ratio of "tail" to "acceleration"
+//        pulseAlgorithm: true,
+//        pulseScale: 4,
+//        pulseNormalize: 1,
+
+//        // Поддержка тачпада
+//        touchpadSupport: true,
+//      });
+// 	  }
 });
